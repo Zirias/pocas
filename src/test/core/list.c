30878e834044c593e435f936f0407b53645f58a3
Felix Palmen <felix@palmen-it.de>
2017-04-11 16:31:47 +0200

    more list tests

--- src/test/core/list.c
+++ src/test/core/list.c
@@ -133,6 +133,28 @@ TESTMETHOD(remove_item)
     Test_pass();
 }
 
+static int stringStartsWithSelector(const void *value, void *arg)
+{
+    const char *start = arg;
+    return !strncmp(start, (const char *)value, strlen(arg));
+}
+
+TESTMETHOD(remove_matching_item)
+{
+    static char *items[] = {"A", "B", "C"};
+    List_append(notOwningList, items[0]);
+    List_append(notOwningList, items[1]);
+    List_append(notOwningList, items[2]);
+
+    List_removeMatching(notOwningList, stringStartsWithSelector, "B");
+
+    Test_assertIntEqual(2, List_length(notOwningList), "wrong item count");
+    Test_assertStrEqual("A", List_get(notOwningList, 0), "unexpected item");
+    Test_assertStrEqual("C", List_get(notOwningList, 1), "unexpected item");
+
+    Test_pass();
+}
+
 TESTMETHOD(invalid_index_gives_0)
 {
     Test_assertRefEqual(0, List_get(notOwningList, -1), "invalid index did not return 0");
@@ -141,22 +163,40 @@ TESTMETHOD(invalid_index_gives_0)
     Test_pass();
 }
 
-static int selector(const void *value, void *arg)
-{
-    (void)arg;
-    return !strncmp("y_", (const char *)value, 2);
-}
-
 TESTMETHOD(select_items)
 {
     List_append(stringList1, String_copy("y_FOO"));
     List_append(stringList1, String_copy("n_BAR"));
     List_append(stringList1, String_copy("y_BAZ"));
 
-    tmpList1 = List_select(stringList1, selector, 0);
+    tmpList1 = List_select(stringList1, stringStartsWithSelector, "y_");
 
     Test_assertIntEqual(2, List_length(tmpList1), "wrong item count");
     Test_assertStrEqual("y_BAZ", List_get(tmpList1, 1), "unexpected item");
+
+    Test_pass();
+}
+
+static void *stringToLengthTransform(const void *element, void *arg)
+{
+    (void)arg;
+    return (void *)(uintptr_t)strlen((const char *)element);
+}
+
+TESTMETHOD(transform_list)
+{
+    List_append(notOwningList, "one");
+    List_append(notOwningList, "two");
+    List_append(notOwningList, "three");
+    List_append(notOwningList, "fourtytwo");
+
+    tmpList1 = List_transform(notOwningList, 0, 0, stringToLengthTransform, 0);
+
+    Test_assertIntEqual(4, List_length(tmpList1), "wrong item count");
+    Test_assertIntEqual(3, (uintptr_t)List_get(tmpList1, 0), "incorrectly transformed");
+    Test_assertIntEqual(3, (uintptr_t)List_get(tmpList1, 1), "incorrectly transformed");
+    Test_assertIntEqual(5, (uintptr_t)List_get(tmpList1, 2), "incorrectly transformed");
+    Test_assertIntEqual(9, (uintptr_t)List_get(tmpList1, 3), "incorrectly transformed");
 
     Test_pass();
 }
